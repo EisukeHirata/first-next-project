fa893f6975304b371aaacbe97098698ffd86b956
平田叡佑 <eisukehirata@hirataakirayuunoMacBook-Pro.local>
2021-07-08 17:59:10 +0900

    First commit from new macbook

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ export const Layout: VFC<{ children: ReactNode }> = (props) => {
   return (
     <div className="grid grid-rows-[auto,1fr,auto] min-h-screen">
       <Header />
-      <main className="bg-red-300">{props.children}</main>
+      <main className="bg-green-300">{props.children}</main>
       <Footer />
     </div>
   );
