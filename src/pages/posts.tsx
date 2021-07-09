f94337b56c974b7b4aa44daf9bacbd12ff2b64bc
平田叡佑 <eisukehirata@hirataakirayuunoMacBook-Pro.local>
2021-07-09 10:19:09 +0900

    add post.tsx

---
+++ src/pages/posts.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { Layout } from "src/components/layout";
+
+const Posts: NextPage = () => {
+  return <Layout>Posts</Layout>;
+};
+
+export default Posts;
