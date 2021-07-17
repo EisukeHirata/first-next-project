1331d4a5840e60690bc50dadcfd1da1400efd91d
EisukeHirata <82617733+EisukeHirata@users.noreply.github.com>
2021-07-17 16:37:19 +0900

    add signup and signin to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,16 +9,26 @@ const items = [
 
 export const Header: VFC = () => {
   return (
-    <header className="items-center h-40">
-      <a className="text-3xl">Tech House Community</a>
-      <nav style={{ float: "right" }}>
-        {items.map(({ href, label }) => {
-          return (
-            <Link key={href} href={href}>
-              <a className="inline-block p-4">{label}</a>
-            </Link>
-          );
-        })}
+    <header className="container px-4 mx-auto">
+      <nav className="flex justify-between items-center py-6">
+        <a className="pr-10 text-3xl font-semibold leading-none">Tech House Community</a>
+        <div className="flex items-center space-x-12 w-auto">
+          {items.map(({ href, label }) => {
+            return (
+              <Link key={href} href={href}>
+                <a className="inline-block text-sm text-gray-400 hover:text-gray-500">{label}</a>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="ml-auto">
+          <a className="inline-block py-3 px-4 mr-2 text-xs font-semibold leading-none text-blue-600 hover:text-blue-700 rounded border border-blue-200 hover:border-blue-300">
+            Log In
+          </a>
+          <a className="inline-block py-3 px-4 text-xs font-semibold leading-none text-white bg-blue-600 hover:bg-blue-700 rounded">
+            Sign Up
+          </a>
+        </div>
       </nav>
     </header>
   );
